408c48c1ac3ff68bd1b1a80203bef82a58ff8d45
Nikhil Dangi <nikhildangi@Nikhils-MacBook-Pro.local>
2024-06-25 02:20:13 +0530

    initial

---
+++ src/constants.js
@@ -0,0 +1,23 @@
+export const scaleFactor = 4;
+
+export const dialogueData = {
+  welcome: `Welcome <a href="https://www.behance.net/nikhildangi1" target="_blank"></a><a href="https://www.behance.net/nikhildangi1" target="_blank"></a> to Nikhil Dangi's 2D game portfolio. 
+  <a href="https://www.behance.net/nikhildangi1" target="_blank"></a><a href="https://www.behance.net/nikhildangi1" target="_blank"></a>Feel free to move around the house 
+  <a href="https://www.behance.net/nikhildangi1" target="_blank"></a><a href="https://www.behance.net/nikhildangi1" target="_blank"></a>and explore different section.`,
+  pc: `This is my PC. I work mostly Data visualizing using Tableau Public and build UI/UX these days.
+    I mostly love to work in area where UX matter most and where every step choosen in a project has its own story to tell.
+    Here is my <a href="https://github.com/ndangi168" target="_blank">Github</a>!`,
+  "cs-degree": `This is my CS degree. I did my B.Tech from Vellore Institute of Technology in Computer Science with Specialization in Business System.`,
+  "sofa-table": `That's my sofa. I like to relax here and watch YouTube. 
+  I also love to do 3D modeling and animation in my free time`,
+  tv: `This is my TV. Checkout my <a href="https://public.tableau.com/app/profile/nikhil.dangi/vizzes" target="_blank">Tableau</a>, 
+  <a href="https://www.behance.net/nikhildangi1" target="_blank">Behance</a>, and <a href="https://github.com/ndangi168" target="_blank">Github</a> profile`,
+  bed: `This where I sleep. Great ideas comes when I'm lying on my bed. When an idea strikes, I often have to write it down or else I won't be able to sleep because my mental energy is consumed by it.`,
+  resume: `This is my desk and on it is my resume. <a href="https://drive.google.com/file/d/11ukUCcTT86T7hPmS3hpTH8N5YW2xCQtW/view?usp=sharing" target="_blank">Check it out?</a>
+  Contact me at <a href= "mailto: nikhildangi168@gmail.com"> nikhildangi168@gmail.com </a> if you have any interesting job opportunities!`,
+  projects: `Info about this 2D game portfolio : Simple game portfolio made using Javascript library "Kaboom.js" which is used to make games. Kaboom.js is no longer maintained.`,
+  library: `That's My Skillshelf which contains<br> 
+  Programing languages: Python, Go lang, C++, JavaScript, SQL, Django, Swift, php, Java.<br>
+  And Developer Tools: Tableau, Docker, Linux, Figma, Adobe Photoshop, Blender, IBM Cloud, Kubernetes`,
+  exit: `If you want to exit Nikhil Dangi's portfolio, just close the tab.`,
+};
---
+++ vite.config.js
@@ -0,0 +1,8 @@
+import { defineConfig  } from "vite";
+
+export default defineConfig({
+    base: "./Nikhil-Dangi-2D-Game-Portfolio/",
+    build: {
+        minify: "terser",
+    },
+});
